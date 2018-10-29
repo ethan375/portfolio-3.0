7650d5e56430c0af49461fcec0118c643c69a038
ethan375 <ethanebel1@gmail.com>
2018-10-28 22:03:49 -0500

    changed sass file names to fix false error, home page setup almost done

--- js/app.js
+++ js/app.js
@@ -4,15 +4,15 @@ const app = angular.module('portfolio', ['ngRoute']);
 app.config(function($routeProvider){
 	$routeProvider
 	.when('/web-development', {
-		templateUrl: './views/web-development.html'
+		templateUrl: '../views/web-development.html'
 	})
 	.when('/reiki', {
-		templateUrl: './views/reiki.html'
+		templateUrl: '../views/reiki.html'
 	})
 	.when('/collectible-coins', {
-		templateUrl: './views/collectible-coins.html'
+		templateUrl: '../views/collectible-coins.html'
 	})
 	.otherwise({
-		templateUrl: './views/home.html'
+		templateUrl: '../views/home.html'
 	})
 })
